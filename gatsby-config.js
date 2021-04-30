74e963ca2ee1b2fe16d6d5ad1167b02e9ae6b9f6
Anthony Englebert <anthonyenglebert@ik.me>
2021-04-30 11:35:08 +0200

    /

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,40 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Coccinelles et compagnies`,
+    description: `site Coccinelles et compagnies`,
+    author: `Coccinelles et compagnies`,
   },
   plugins: [
+
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: `4bc27cba78206fcf7af0cf18aa3816`,
+  
+        // The project environment to read from. Defaults to the primary environment:
+        environment: `main`,
+  
+        // If you are working on development/staging environment, you might want to
+        // preview the latest version of records instead of the published one:
+        previewMode: false,
+  
+        // Disable automatic reloading of content when some change occurs on DatoCMS:
+        disableLiveReload: false,
+  
+        // Custom API base URL (most don't need this)
+        // apiUrl: 'https://site-api.datocms.com',
+  
+        // Setup locale fallbacks
+        // In this example, if some field value is missing in Italian, fall back to English
+        localeFallbacks: {
+          it: ['fr'],
+        },
+      },
+    },
+
+
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
