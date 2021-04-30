69a01f6878428f20669da156dfd8b88154af816d
Anthony Englebert <anthonyenglebert@ik.me>
2021-04-30 12:05:13 +0200

    /

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { HelmetDatoCms } from 'gatsby-source-datocms'
 
-const IndexPage = ({ data }) => 
-<h1>
-  coucou
-  {data.allDatoCmsComingSoon.edges.node.titre}
-</h1>
+const IndexPage = () => (
+<>
+    <Seo title="Home" />
+    <h1>Un atelier pour créer, vivre et partager.</h1>
+    <StaticImage
+      src="../images/logo-titres.svg"
+      width={500}
+      quality={100}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
 
 
-  
-export const query = graphql`
-  {
-    allDatoCmsComingSoon {
-      edges {
-        node {
-          logo {
-            url
-          }
-          texte
-          titre
-          slogan
-        }
-      }
-    }
-  }
-`
+<h2>Une boutique d'objets éthiques et durables</h2>
+<p></p>
+</>
+)
+
+
 export default IndexPage
